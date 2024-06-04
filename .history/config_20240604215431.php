ff4bfdee3b8950d06039abd13adbf3e61de5dc6f
Tuan Anh <tunanguyenlc@gmail.com>
2024-06-04 21:55:04 +0700

    update db

--- .history/config_20240604215431.php
+++ .history/config_20240604215431.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'tuananhdb.cvufoposxbbs.us-east-1.rds.amazonaws.com');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'Nguyentuananh03');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'clouddb');
 
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
